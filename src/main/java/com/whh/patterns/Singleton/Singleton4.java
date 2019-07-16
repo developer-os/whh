1f666091c2767cc402ce56412d54be7840804eae
rchen7 <rocky.chen@morningstar.com>
2019-07-16 17:32:31 +0800

    commit

--- src/main/java/com/whh/patterns/Singleton/Singleton4.java
+++ src/main/java/com/whh/patterns/Singleton/Singleton4.java
@@ -3,15 +3,20 @@ package com.whh.patterns.Singleton;
 /**
  * 
  * @desc:单例
- *  非 lazy init
- *  这里final不是必须的.
-
-static为我们提供了保证(正确的被创建, 创建的对象是完整的) 可以参考:JSR 133 (Java Memory Model) FAQ
+ * 
  */
 public class Singleton4 {
-	private static final Singleton4 INSTANCE1 = new Singleton4();
+	private static Singleton4 INSTANCE1 = null;
+
 	static Singleton4 getInstance() {
+		if (INSTANCE1 == null) {
+			synchronized (Singleton4.class) {
+				if (INSTANCE1 == null) {
+					INSTANCE1 = new Singleton4();
+				}
+			}
+		}
 		return INSTANCE1;
 	}
-	
+
 }
